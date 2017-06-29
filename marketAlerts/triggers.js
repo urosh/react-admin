56e7e6355682638522e73e1e11e92128833845f2
urosh <uros.damnjanovic@gmail.com>
2017-06-29 10:32:40 +0300

    Registering market alerts events to the system

--- marketAlerts/triggers.js
+++ marketAlerts/triggers.js
@@ -24,7 +24,6 @@ module.exports = (marketAlerts) => {
 			marketAlertsParameters.LAST_EVENT_DATE,
 			marketAlertsParameters.DIFFERENCE,
 			marketAlertsParameters.EVENT_DESCRIPTION
-			
 		],
 		function(data) {
 			let processedData = marketAlertTranslate(data);
@@ -33,6 +32,34 @@ module.exports = (marketAlerts) => {
 		},
 		'post',
 		'/live/market-trigger'
+	)
+
+	marketAlerts.addEvent(
+		'marketAlertTriggerTest',
+		generalConfig.eventChannels.ROUTES,
+		[
+			marketAlertsParameters.ROW_ID,
+			marketAlertsParameters.EVENT_ID,
+			marketAlertsParameters.EVENT_DATE,
+			marketAlertsParameters.BASE_CURR,
+			marketAlertsParameters.NON_BASE_CURR,
+			marketAlertsParameters.EVENT_TYPE_ID,
+			marketAlertsParameters.NEW_VALUE,
+			marketAlertsParameters.OLD_VALUE,
+			marketAlertsParameters.LAST_EVENT_DATE,
+			marketAlertsParameters.DIFFERENCE,
+			marketAlertsParameters.EVENT_DESCRIPTION
+		],
+		function(data) {
+			data[parametersList.TEST_ENABLED] = true;
+			let processedData = marketAlertTranslate(data);
+			console.log('We recieved market alert');
+			console.log(processedData);
+		},
+		'post',
+		'/live/market-trigger/test'
 	)	
+
+
 }
 
