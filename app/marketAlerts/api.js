d3dc881e105ebff1c4c96c0b5eb136d540a9745f
urosh <uros.damnjanovic@gmail.com>
2017-07-05 16:02:37 +0300

    Adding api routes

--- app/marketAlerts/api.js
+++ app/marketAlerts/api.js
@@ -18,19 +18,62 @@ module.exports  = (marketAlerts, usersManagement) => {
 		},
 		'get'
 	)
+	
+	// Api methods for retrieving list of logged out users
+	marketAlerts.addEvent(
+		'/api/fetch/visitors',
+		config.eventChannels.ROUTES,
+		[],
+		function(req, res) {
+			const users = usersManagement.getUsers();
+			const loggedOutUsers = Object.keys(users)
+				.map(id => users[id])
+				.filter(user => !user[parametersList.USER_ID]);
+			res.send(loggedOutUsers);
+		},
+		'get'
+	)
+	
 
+	// Api method for retrieving a list of users with push notifications enabled
 	marketAlerts.addEvent(
 		'/api/fetch/push',
 		config.eventChannels.ROUTES,
 		[],
 		function(req, res) {
 			const users = usersManagement.getUsers();
-			const loggedInUsers = Object.keys(users)
+			const pushUsers = Object.keys(users)
 				.map(id => users[id])
 				.filter(user => user[parametersList.PUSH].length);
-			res.send(loggedInUsers);
+			res.send(pushUsers);
 		},
 		'get'
 	)
+	
+	// Api method for retrieving a list of mobile app users
+	marketAlerts.addEvent(
+		'/api/fetch/mobiles',
+		config.eventChannels.ROUTES,
+		[],
+		function(req, res) {
+			const users = usersManagement.getUsers();
+			const mobileUsers = Object.keys(users)
+				.map(id => users[id])
+				.filter(user => user[parametersList.MOBILES].length);
+			res.send(mobileUsers);
+		},
+		'get'
+	)
+	
+	marketAlerts.addEvent(
+		'/test',
+		config.eventChannels.ROUTES,
+		[],
+		function(req, res) {
+			res.send(usersManagement.getUsers());
+		},
+		'get'
+	)
+
 
 }
