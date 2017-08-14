f76c45dba778a4dea5ec752d8d753e03c1d99ecc
urosh <uros.damnjanovic@gmail.com>
2017-08-14 09:11:09 +0300

    Referencing redis handler module

--- app/clients/index.js
+++ app/clients/index.js
@@ -1,6 +1,16 @@
+/*
+ * Group of events related to managing clients connections. 
+ * Currently browser sockets/push notifications and mobile device connections are 
+ * supported. 
+ * 
+ * Browsers are communicating using sockets, while mobile app uses http rest api
+ * to communication to the server. 
+ * 
+ */
 module.exports = (clients, usersManagement) => {
 	require('./browserConnections')(clients, usersManagement);
 	require('./mobileConnections')(clients, usersManagement);
 	require('./pushConnections')(clients, usersManagement);
 	require('./api')(clients,usersManagement);
+	require('./redisEvent')(clients,usersManagement);
 }
